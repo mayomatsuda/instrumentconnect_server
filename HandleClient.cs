7dbd19cffc076bbe1ac1d243a9c8579e2c0c5ba1
Myles MacDonald <mylesmac12@gmail.com>
2020-06-10 09:57:12 -0400

    Initial commit

---
+++ HandleClient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections;
+
+namespace Server
+{
+    public class HandleClient
+    {
+        TcpClient clientSocket;
+        string clNo;
+        Hashtable clientsList;
+
+        public void StartClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+        {
+            this.clientSocket = inClientSocket;
+            this.clNo = clineNo;
+            this.clientsList = cList;
+            Thread ctThread = new Thread(Listen);
+            ctThread.Start();
+        }
+
+        private void Listen()
+        {
+            byte[] bytesFrom = new byte[1024];
+            string dataFromClient;
+            bool connected = true;
+
+            while (connected)
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    networkStream.Read(bytesFrom, 0, 1024);
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                    string[] sData = dataFromClient.Split('-');
+                    dataFromClient = sData[0];
+                    Console.WriteLine(dataFromClient);
+
+                    Server.Broadcast(dataFromClient, clNo, true);
+                    networkStream.Flush();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    connected = false;
+                }
+            }
+            Server.clientsList.Remove(clNo);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+using System.Net;
+
+namespace Server
+{
+    class Server
+    {
+        public static Hashtable clientsList = new Hashtable();
+
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(IPAddress.Parse("127.0.0.1"), 11111);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+
+            serverSocket.Start();
+            Console.WriteLine("Server Started...");
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                byte[] bytesFrom = new byte[1024];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, 1024);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                Console.Write(dataFromClient);
+
+                try
+                {
+                    clientsList.Add(dataFromClient, clientSocket);
+                }
+                catch
+                {
+                    Console.WriteLine("Client already added");
+                }
+                HandleClient client = new HandleClient();
+                client.StartClient(clientSocket, dataFromClient, clientsList);
+            }
+        }
+
+        public static void Broadcast(string msg, string uName, bool flag)
+        {
+            foreach (DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+
+                byte[] broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+    }
+}
